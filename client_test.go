c7573ec700d10b4bc0af6af87c1038ec8100f4dc
Nathan Osman <nathan@quickmediasolutions.com>
2023-01-07 01:38:32 -0800

    Begin adding tests for Client.

---
+++ client_test.go
@@ -0,0 +1,34 @@
+package hue
+
+import (
+	"testing"
+
+	"github.com/lampctl/go-hue/bridge/bridgetest"
+)
+
+func TestClient(t *testing.T) {
+	for _, v := range []struct {
+		Name string
+		Fn   func(c *Client, s *bridgetest.Bridge) error
+		Err  error
+	}{
+		{
+			Name: "unauthenticated request",
+			Fn: func(c *Client, s *bridgetest.Bridge) error {
+				_, err := c.Resources()
+				return err
+			},
+			Err: errForbiddenResponse,
+		},
+	} {
+		s, err := bridgetest.New()
+		if err != nil {
+			t.Fatalf("%s: %s", v.Name, err)
+		}
+		defer s.Close()
+		c := &Client{Host: s.URL}
+		if err := v.Fn(c, s); err != v.Err {
+			t.Fatalf("%s: %#v != %#v", v.Name, err, v.Err)
+		}
+	}
+}
