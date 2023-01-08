1d83c451b7f829e5b3923f1db6de06b2afe45bad
Nathan Osman <nathan@quickmediasolutions.com>
2023-01-07 17:35:37 -0800

    Refactor error checking in tests.

--- client_test.go
+++ client_test.go
@@ -1,6 +1,8 @@
 package hue
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/lampctl/go-hue/bridge/bridgetest"
@@ -8,17 +10,49 @@ import (
 
 func TestClient(t *testing.T) {
 	for _, v := range []struct {
-		Name string
-		Fn   func(c *Client, s *bridgetest.Bridge) error
-		Err  error
+		Name        string
+		Fn          func(c *Client, s *bridgetest.Bridge) error
+		ReturnError bool
 	}{
+		{
+			Name: "register without button press",
+			Fn: func(c *Client, s *bridgetest.Bridge) error {
+				return c.Register("")
+			},
+			ReturnError: true,
+		},
+		{
+			Name: "register with only button press",
+			Fn: func(c *Client, s *bridgetest.Bridge) error {
+				s.PushButton()
+				return c.Register("")
+			},
+			ReturnError: true,
+		},
+		{
+			Name: "register with call and button press",
+			Fn: func(c *Client, s *bridgetest.Bridge) error {
+				if err := c.Register(""); err == nil {
+					return errors.New("error expected without button press")
+				}
+				s.PushButton()
+				if err := c.Register(""); err != nil {
+					return err
+				}
+				if c.Username != bridgetest.Username {
+					return fmt.Errorf("%+v != %+v", c.Username, bridgetest.Username)
+				}
+				return nil
+			},
+			ReturnError: false,
+		},
 		{
 			Name: "unauthenticated request",
 			Fn: func(c *Client, s *bridgetest.Bridge) error {
 				_, err := c.Resources()
 				return err
 			},
-			Err: errForbiddenResponse,
+			ReturnError: true,
 		},
 		{
 			Name: "authenticated request",
@@ -27,7 +61,7 @@ func TestClient(t *testing.T) {
 				_, err := c.Resources()
 				return err
 			},
-			Err: nil,
+			ReturnError: false,
 		},
 	} {
 		s, err := bridgetest.New()
@@ -36,8 +70,16 @@ func TestClient(t *testing.T) {
 		}
 		defer s.Close()
 		c := &Client{Host: s.URL}
-		if err := v.Fn(c, s); err != v.Err {
-			t.Fatalf("%s: %+v != %+v", v.Name, err, v.Err)
+		var (
+			fnErr       = v.Fn(c, s)
+			returnError = fnErr != nil
+		)
+		if returnError != v.ReturnError {
+			if v.ReturnError {
+				t.Fatalf("%s: %s", v.Name, "error expected")
+			} else {
+				t.Fatalf("%s: %s", v.Name, fnErr)
+			}
 		}
 	}
 }
